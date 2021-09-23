23d065259a540b1ffb1c3fe91fc8681125faed37
Gene Zhirnov <genezhirnov@gmail.com>
2021-09-23 15:50:29 -0400

    c# animal stuff

---
+++ Animal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public double Age { get; set; }
+        public string Type { get; set; }
+        public double WeightInLbs { get; set; }
+
+        private string AgeInYears()
+        {
+            var intAge = Math.Floor(Age);
+            var monthCount = Math.Floor((Age - intAge) * 12);
+            return $"{intAge:N0} years, {monthCount:N0} months old";
+        }
+
+        public override string ToString()
+        {
+            return $"I'm {Name} and I'm a {Type}. I weigh {Math.Round(WeightInLbs, 2):N2} lbs and I'm {AgeInYears()}; thank you for asking.";
+        }
+        //private int _age;
+        //public int Age
+        //{
+        //    get
+        //    {
+        //        return _age;
+        //    }
+        //    private set
+        //    {
+        //        if (value <= 120 && value > 0)
+        //        {
+        //            _age = value;
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static string[] ANIMAL_TYPES = new string[] { "Hippopotamus", "Red Panda", "Penguin", "Flamingo" };
+
+        private static double randomGenerator(Random r, double min, double max)
+        {
+            return (r.NextDouble() * (max - min)) + min;
+        }
+
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var animalList = new List<Animal>();
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter a name for your favourite animal:");
+                string animalName = Console.ReadLine();
+                var animal = new Animal()
+                {
+                    Name = animalName,
+                    Age = randomGenerator(rnd, 0.0d, 120d),
+                    Type = ANIMAL_TYPES[Convert.ToInt32(Math.Floor(randomGenerator(rnd, 0, 4)))],
+                    WeightInLbs = randomGenerator(rnd, 300d, 4800.5d)
+                };
+                //add method to add animals
+                animalList.Add(animal);
+                Console.Write("Have you had enough animal action? (Y if so)\t");
+            } while (Console.ReadKey().Key != ConsoleKey.Y);
+            Console.WriteLine();
+            animalList.ForEach(Console.WriteLine);
+            var zoo = animalList.ToDictionary(a => a.Type, a => a);
+
+            for (int x = 0; x < zoo.Count; x++)
+            {
+                Console.WriteLine("zero index: {0} and one index: {1} ", zoo.Keys.ElementAt(x));
+                                    
+            }
+
+
+
+
+        }
+    }
+    
+}
